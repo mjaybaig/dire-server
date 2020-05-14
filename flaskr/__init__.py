35445b0716a7ed8111197abf3bce2dfc976a8df9
M. Jameel Baig <mjameelbaig@hotmail.com>
2020-05-15 01:01:11 +1000

    Add some other change

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -44,11 +44,11 @@ def get_uv_data(date):
 
 # def create_app(test_config=None):
 # print("Hello")
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
-)
+app = Flask(__name__)
+# app.config.from_mapping(
+#     SECRET_KEY='dev',
+#     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+# )
 
 # if test_config is None:
 #     app.config.from_pyfile('config.py', silent=True)
