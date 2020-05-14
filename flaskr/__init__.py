3a3d8d8e948a1e48234251f3948583ef91f171bc
M. Jameel Baig <mjameelbaig@hotmail.com>
2020-05-15 00:50:44 +1000

    Change to server

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -42,27 +42,30 @@ def get_uv_data(date):
     }
     return response
 
-def create_app(test_config=None):
-    print("Hello")
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
-    )
+# def create_app(test_config=None):
+# print("Hello")
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+)
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+# if test_config is None:
+#     app.config.from_pyfile('config.py', silent=True)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# try:
+#     os.makedirs(app.instance_path)
+# except OSError:
+#     pass
 
 
-    @app.route('/')
-    def hello():
-        print("recieved request")
-        print()
-        return json.dumps(get_uv_data(request.args.get('date')))
-    
-    return app
+@app.route('/')
+def hello():
+    print("recieved request")
+    print()
+    return json.dumps(get_uv_data(request.args.get('date')))
+
+# return app
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
